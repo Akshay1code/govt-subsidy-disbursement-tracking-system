71f3306283fa5340e717924b76ec59890c3c3a28
Bara Jeevan Manoj Sai <jeevanmanojsai14@gmail.com>
2026-08-06 21:30:36 +0530

    Remove old disbursement module

--- backend/src/main/java/com/example/gov_scheme_backend/disbursement/repository/DisbursementPlanRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.gov_scheme_backend.disbursement.repository;
-
-import com.example.gov_scheme_backend.disbursement.model.DisbursementPlan;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DisbursementPlanRepository extends JpaRepository<DisbursementPlan, Long> {
-}
