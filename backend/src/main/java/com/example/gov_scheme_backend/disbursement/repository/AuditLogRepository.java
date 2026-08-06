4a062d47920d9cac82dae417739ec55ff7b14676
Bara Jeevan Manoj Sai <jeevanmanojsai14@gmail.com>
2026-08-06 21:30:36 +0530

    Remove old disbursement module

--- backend/src/main/java/com/example/gov_scheme_backend/disbursement/repository/AuditLogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.gov_scheme_backend.disbursement.repository;
-
-import com.example.gov_scheme_backend.disbursement.model.AuditLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
-}
